20f7b0250bcc54893644bf484797f508fea58ead
realmizzer <diamondAlma32@gmail.com>
2025-06-22 16:34:44 +0700

    moved fronted files into the client folder

--- src/shared/theme/palettes.ts
+++
@@ -1,9 +0,0 @@
-import { Theme } from '@/shared/theme/theme.ts';
-import { IThemeColor } from '@/shared/theme/colors/IThemeColor';
-import { LightThemeColors } from '@/shared/theme/colors/LightThemeColors';
-import { DarkThemeColors } from '@/shared/theme/colors/DarkThemeColors';
-
-export const palettes: Record<Theme, IThemeColor> = {
-  light: LightThemeColors,
-  dark: DarkThemeColors,
-};
