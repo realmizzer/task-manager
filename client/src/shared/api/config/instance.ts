dd663fe755e7bc2898278fc788ce93640bca89fe
realmizzer <diamondAlma32@gmail.com>
2025-06-23 12:03:51 +0700

    fix: no api url on the android devices

--- client/src/shared/api/config/instance.ts
+++ client/src/shared/api/config/instance.ts
@@ -2,5 +2,5 @@ import axios from 'axios';
 import Config from 'react-native-config';
 
 export const api = axios.create({
-  baseURL: Config.API_URL,
+  baseURL: Config.API_URL ?? 'http://127.0.0.1:3001',
 });
